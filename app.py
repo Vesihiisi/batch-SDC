3f4112a6318b79e966d781b9a4862b0405e29b00
Vesihiisi <alicia@fagerving.se>
2020-12-23 09:49:12 +0100

    upgrades

--- app.py
+++ app.py
@@ -1,99 +1,206 @@
+import argparse
 import csv
 import json
-import time
 import urllib.parse
 import pywikibot
 
-import pywikibot.config2 as config
-config.put_throttle = 5
+PROPERTIES = {"P369": {"datatype": "qid"},
+              "P6216": {"datatype": "qid"},
+              "P518": {"datatype": "qid"},
+              "P2093": {"datatype": "string"}}
 
-with open('data.csv', 'r') as f:
-    data = list(csv.reader(f))
+# PROPERTIES = {"P6216": {"datatype": "qid"}, "P1957": {
+#     "datatype": "string"}}
+# "P369" # sandbox property for value of type "Item"
+# "P1957"  # wikisource index (string) -- works!
 
-site = pywikibot.Site('commons', 'commons')
-site.login()
-site.get_tokens('csrf')
-repo = site.data_repository()
+# value={"entity-type":"item","numeric-id":1}
+# value="itsastring"
 
-def add_claim_json(pid, value):
-    """
-    Add a claim to a mediaid
-    :param mediaid: The mediaid to add it to
-    :param pid: The property P id (including the P)
-    :param value: string value
-    :param summary: The summary to add in the edit
-    :return: Nothing, edit in place
-    """
-    toclaim = {
-    'claims': [{
-        'mainsnak':{
-            'snaktype':'value',
-            'property':pid,
-            'datavalue':{
-                'value':value,
-                'type':'string',
-                }
-            },
-            'type':'statement',
-            'rank':'normal',
-            'qualifiers-order': ['P195'],
-            'qualifiers':{
-                'P195': [{
-                    'snaktype':'value',
-                    'property':'P195',
-                    'datavalue': {
-                        'value': {
-                            'entity-type': 'item',
-                            'numeric-id': 1142142,
-                            'id': 'Q1142142',
-                        },
-                        'type':'wikibase-entityid',
-                    }
-                }]
-            }
-        }]
+# Q29870196 70 years after death
+
+# {
+#       'type' : 'wikibase-entityid',
+#       'value': {
+#         'numeric-id': '80151',
+#         'id' : 'Q80151',
+#       },
+
+# CAPTION https://www.wikidata.org/w/api.php?action=help&modules=wbsetlabel
+# api.php?action=wbsetlabel&site=enwiki&title=Earth&language=en&value=Earth
+
+
+def add_caption_json(language, content):
+    caption = {
+        'language': language,
+        'value': content
     }
-    return toclaim
+    return caption
 
-def getCurrentMediaInfo(mediaid):
+
+def create_datavalue(value, valuetype):
+    if valuetype == "qid":
+        datavalue = {
+            'value': {
+                'numeric-id': value[1:],
+                'id': value,
+            },
+            'type': 'wikibase-entityid',
+        }
+    elif valuetype == "string":
+        datavalue = {
+            'value': value,
+            'type': 'string',
+        }
+    return datavalue
+
+
+def create_claim_json(pid, value, valuetype):
+    datavalue = create_datavalue(value, valuetype)
+    newclaim = {
+        'mainsnak': {
+            'snaktype': 'value',
+            'property': pid,
+            'datavalue': datavalue,
+        },
+        'type': 'statement',
+        'rank': 'normal',
+        'qualifiers-order': [],
+        'qualifiers': {}
+    }
+    return newclaim
+
+
+def get_current_mediainfo(mediaid):
     """
-    Check if the media info exists. If that's the case, return that so we can expand it.
+    Check if the media info exists.
+    If that's the case, return that so we can expand it.
     Otherwise return an empty dict
     :param mediaid: The entity ID (like M1234, pageid prefixed with M)
     :return: json
     """
+    site = pywikibot.Site('commons', 'commons')
     request = site._simple_request(action='wbgetentities', ids=mediaid)
     data = request.submit()
     if data.get(u'entities').get(mediaid).get(u'pageid'):
         return data.get(u'entities').get(mediaid)
     return {}
 
-def write_statement(json_data, mid, summary):
+
+def write_caption(json_data, mid, summary):
+    site = pywikibot.Site('commons', 'commons')
     token = site.tokens['csrf']
-    post_data = {'action' : 'wbeditentity',
-                'format' : 'json',
-                'id' : mid,
-                'data' : json.dumps(json_data, separators=(',', ':')),
-                'token' : token,
-                'summary': summary,
-                'bot' : True,
-                }
+    post_data = {
+        'action': 'wbsetlabel',
+        'format': 'json',
+        'id': mid,
+        'value': json_data.get("value"),
+        'summary': summary,
+        'language': json_data.get("language"),
+        'bot': True,
+        'token': token,
+    }
     request = site._simple_request(**post_data)
     print(request)
     try:
         data = request.submit()
     except pywikibot.data.api.APIError:
-        pywikibot.output('Got an API error while saving page. Sleeping and skipping')
-        time.sleep(60)
+        pywikibot.output(
+            'Got an API error while saving page. Sleeping and skipping')
 
-for item in data:
-    page = pywikibot.Page(site, title='{}'.format(urllib.parse.quote(item[1])), ns=6)
-    mid = 'M' + str(page.pageid)
-    nm_identifier = item[2]
-    current_data = getCurrentMediaInfo(mid)
-    if current_data.get('statements'):
-        if current_data.get('statements').get('P217'):
-            continue
-    json_data = add_claim_json('P217', nm_identifier)
-    summary = "Adding identifier ID in Nordic Museum collection"
-    write_statement(json_data, mid, summary)
+
+def write_statement(json_data, mid, summary):
+    site = pywikibot.Site('commons', 'commons')
+    token = site.tokens['csrf']
+    post_data = {'action': 'wbeditentity',
+                 'format': 'json',
+                 'id': mid,
+                 'data': json.dumps(json_data, separators=(',', ':')),
+                 'token': token,
+                 'summary': summary,
+                 'bot': True,
+                 }
+    request = site._simple_request(**post_data)
+    print(request)
+    try:
+        data = request.submit()
+    except pywikibot.data.api.APIError:
+        pywikibot.output(
+            'Got an API error while saving page. Sleeping and skipping')
+
+
+def read_data(filename):
+    datalist = []
+    with open(filename, 'r') as data:
+        for line in csv.DictReader(data):
+            datalist.append(line)
+    return datalist
+
+
+def main(arguments):
+    site = pywikibot.Site('commons', 'commons')
+    data = read_data(arguments.get("data"))
+    for row in data:
+        claims_to_add = {'claims': []}
+        filename = row.get("Filename")
+        page = pywikibot.Page(site, title='{}'.format(
+            urllib.parse.quote(filename)), ns=6)
+        mid = 'M' + str(page.pageid)
+        for key in row.keys():
+            if key.startswith("Caption|"):
+                language = key.split("|")[1]
+                content = row[key]
+                json_data = add_caption_json(language, content)
+                summary = "...test... Adding caption: {}".format(row[key])
+                # write_caption(json_data, mid, summary)
+            if key.startswith("P"):
+                property_with_qualifiers = key.split("|")
+                main_property = property_with_qualifiers[0]
+                main_value = row[key].split("|")[0]
+
+                qualifier_properties = property_with_qualifiers[1:]
+                qualifier_values = row[key].split("|")[1:]
+
+                claim_data = create_claim_json(
+                    main_property, main_value,
+                    valuetype=PROPERTIES[main_property].get("datatype"))
+
+                if qualifier_properties:
+                    qual_dict = {qualifier_properties[i]: qualifier_values[i]
+                                 for i in range(
+                        len(qualifier_properties))}
+                    claim_data = add_qualifiers_to_claim(claim_data, qual_dict)
+                claims_to_add["claims"].append(claim_data)
+        edit_comment = create_edit_comment(claims_to_add)
+        print(edit_comment)
+        write_statement(claims_to_add, mid, edit_comment)
+
+
+def create_edit_comment(claims_to_add):
+    properties = []
+    base = "Adding {}."
+    for claim in claims_to_add["claims"]:
+        properties.append("d:" + claim["mainsnak"]["property"])
+    joined = ', '.join(properties)
+    return base.format(joined)
+
+
+def add_qualifiers_to_claim(claim_data, qual_dict):
+    for prop in qual_dict:
+        valuetype = PROPERTIES[prop].get("datatype")
+        datavalue = create_datavalue(qual_dict[prop], valuetype)
+        qualifier = [{
+            'snaktype': 'value',
+            'property': prop,
+            'datavalue': datavalue
+        }]
+        claim_data["qualifiers"][prop] = qualifier
+        claim_data["qualifiers-order"].append(prop)
+    return claim_data
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data", required=True)
+    args = parser.parse_args()
+    main(vars(args))
